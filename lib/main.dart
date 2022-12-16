27485f38236a3d93502fba9c671818e660adb982
Tungbo <1023589809@qq.com>
2022-12-16 21:11:19 +0800

    添加文字淡入淡出效果

--- lib/main.dart
+++ lib/main.dart
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,31 +39,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _opacity = 0.0;
+  bool _isClick = false;
 
-  void _incrementCounter() {
+  void _increment() {
+    _isClick = true;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _opacity = _opacity == 1.0 ? 0.0 : 1.0;
+    });
+  }
+
+  void _callBack() {
+    if(!_isClick) return;
+    _isClick = false;
+    setState(() {
+      _opacity = _opacity == 1.0 ? 0.0 : 1.0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
+
           title: Text(widget.title),
         ),
         body: Container(
@@ -81,18 +71,19 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  const Text(
-                    '当前功德:',
-                    style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50),
-                  ),
-                  Text(
-                    '$_counter',
-                    style: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50),
+                  AnimatedOpacity(
+                    opacity: _opacity,
+                    duration: const Duration(seconds: 1),
+                    onEnd: _callBack,
+                    child: const Text(
+                      '功德+1',
+                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50),
+                    ),
                   ),
                   InkWell(
+                    onTap: _increment,
                     child: Image.asset('images/woodenfish.jpg',
                         width: 210, height: 210),
-                    onTap: _incrementCounter,
                   )
                 ],
               ),
